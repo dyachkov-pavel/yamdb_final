f477dc8f26bc5c96f248ff603eb05a997a84855d
dyachkov-pavel <pahanthebest12345@mail.ru>
2021-04-28 21:13:55 +0300

    flake8 #2

--- apiv1/permissions.py
+++ apiv1/permissions.py
@@ -19,8 +19,8 @@ class IsSafeOrAdminPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.is_authenticated and
-                    request.user.is_admin))
+                or (request.user.is_authenticated
+                    and request.user.is_admin))
 
 
 class IsAuthorOrAbovePermission(permissions.BasePermission):
@@ -31,5 +31,5 @@ class IsAuthorOrAbovePermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method not in ('PUT', 'PATCH', "DELETE")
                 or request.user == obj.author
-                or (request.user.is_authenticated and
-                request.user.is_moderator))
+                or (request.user.is_authenticated
+                and request.user.is_moderator))
